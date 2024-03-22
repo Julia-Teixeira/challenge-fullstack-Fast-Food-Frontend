dbf478815e4853d8758327a0e3c60e455fa8d0a0
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-03-22 10:29:04 -0300

    feat: add search component

---
+++ src/components/search/index.tsx
@@ -0,0 +1,11 @@
+const Search = () => {
+  return (
+    <input
+      title="Pesquisar"
+      placeholder="O que você procura?"
+      className="bg-[#F4F4F4] p-3 rounded w-96 outline-none text-black text-sm mt-4"
+    />
+  );
+};
+
+export default Search;
