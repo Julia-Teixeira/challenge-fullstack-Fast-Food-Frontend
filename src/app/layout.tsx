36a426a8af875a6b978131e3337f8d02038e34bb
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-03-22 10:32:58 -0300

    feat: add header component on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
+import { ProductProvider } from "@/provider/productProvider";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>
+        <ProductProvider>
+          <Header />
+          <main className="flex min-h-screen flex-col px-2 sm:px-6 lg:px-36 mx-auto max-w-7xl">
+            {children}
+          </main>
+        </ProductProvider>
+      </body>
     </html>
   );
 }
