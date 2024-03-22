8d04faca1dbf2773632d432113ff5879739db5be
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-03-22 13:03:16 -0300

    feat: adds the search function

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,10 +1,49 @@
+"use client";
+
+import { useProduct } from "@/provider/productProvider";
+import { TProduct } from "@/provider/productProvider/interface";
+import { useEffect, useState } from "react";
+import { nullable } from "zod";
+
 const Search = () => {
+  const { setSelectedProducts, products, selectedProducts } = useProduct();
+  const [search, setSearch] = useState<string>("");
+  const handleSearch = () => {
+    const filter: TProduct[] | undefined = products?.filter(
+      (product: TProduct) =>
+        product.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setSelectedProducts(filter);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [search]);
+
+  const clearFilter = () => {
+    setSearch("");
+    setSelectedProducts(undefined);
+  };
+
   return (
-    <input
-      title="Pesquisar"
-      placeholder="O que você procura?"
-      className="bg-[#F4F4F4] p-3 rounded w-96 outline-none text-black text-sm mt-4"
-    />
+    <>
+      <input
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+        title="Pesquisar"
+        placeholder="O que você procura?"
+        className="bg-[#F4F4F4] p-3 rounded w-96 outline-none text-black text-sm mt-4"
+      />
+      {selectedProducts !== undefined && selectedProducts?.length > 0 ? (
+        <span
+          className="text-xs underline text-black cursor-pointer mt-1"
+          onClick={() => clearFilter()}
+        >
+          Limpar filtro
+        </span>
+      ) : null}
+    </>
   );
 };
 
