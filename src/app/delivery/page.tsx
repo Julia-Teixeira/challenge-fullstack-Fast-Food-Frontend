1ac12cdec6933c789f331622776f630f95c375f2
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-03-23 19:30:15 -0300

    feat: adds functionality to the 'Retirada' page

--- src/app/delivery/page.tsx
+++ src/app/delivery/page.tsx
@@ -1,5 +1,46 @@
+"use client";
+import { useOrder } from "@/provider/orderProvider";
+import Image from "next/image";
+import { IoMdCheckmark, IoMdClose } from "react-icons/io";
+
 const Delivery = () => {
-  return <h1>Delivery</h1>;
+  const { orders, changeStatusOrder } = useOrder();
+  const ordersOnGoing = orders?.filter((order) => order.status === "onGoing");
+  const ordersFinished = orders?.filter((order) => order.status === "finished");
+
+  return (
+    <section className="flex justify-between text-black h-[600px] mt-8">
+      <div>
+        <h2 className="font-bold text-3xl">Preparando:</h2>
+        <ul className="flex flex-col gap-4 mt-4 overflow-auto h-[400px] w-96">
+          {ordersOnGoing?.map((order) => (
+            <li key={order.id}>
+              <span className="font-semibold text-6xl text-gray-500">
+                {order.nameCostumer}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="h-full w-[5px] bg-black rounded" />
+
+      <div>
+        <h2 className="font-bold text-3xl">Pronto:</h2>
+        <ul className="flex flex-col gap-4 mt-4 overflow-auto h-[400px] w-96">
+          {ordersFinished?.map((order) => (
+            <li key={order.id}>
+              <div>
+                <span className="font-semibold text-6xl text-green-800">
+                  {order.nameCostumer}
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Delivery;
