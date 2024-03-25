77fb21f7ab0b31ea3820178e63aa111d9080a8a2
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-03-25 20:45:45 -0300

    feat: adds images and new libs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-// import cover from "./public/product-cover.svg";
 
 const config: Config = {
   content: [
@@ -13,7 +12,6 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        // "product-cover": `url(${cover})`,
       },
     },
   },
